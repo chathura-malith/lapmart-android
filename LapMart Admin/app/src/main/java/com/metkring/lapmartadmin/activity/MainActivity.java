0441db6c17f8c05af46060a3ddfd6a6d2a52ba80
Chathura-Malith <153283861+chathurama@users.noreply.github.com>
2026-03-24 02:11:46 +0530

    feat: implement admin authentication and main navigation
    - Added Splash screen and SignIn UI.
    - Integrated Firebase Authentication for admin login.
    - Implemented MainActivity with Bottom Navigation setup.
    - Added Toasty library for enhanced UI alerts.

---
+++ LapMart Admin/app/src/main/java/com/metkring/lapmartadmin/activity/MainActivity.java
@@ -0,0 +1,87 @@
+package com.metkring.lapmartadmin.activity;
+
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.metkring.lapmartadmin.R;
+import com.metkring.lapmartadmin.databinding.ActivityMainBinding;
+import com.metkring.lapmartadmin.fragment.AddProductFragment;
+import com.metkring.lapmartadmin.fragment.HomeFragment;
+import com.metkring.lapmartadmin.fragment.ReportFragment;
+import com.metkring.lapmartadmin.fragment.StockFragment;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ActivityMainBinding binding;
+    private BottomNavigationView bottomNavigationView;
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        bottomNavigationView=binding.bottomNavigationView;
+        drawerLayout=binding.drawerLayout;
+
+        bottomNavigationView.setOnItemSelectedListener(this::onNavigationItemSelected);
+
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragment());
+            bottomNavigationView.setSelectedItemId(R.id.bottom_nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        Menu bottomNavMenu = bottomNavigationView.getMenu();
+
+        for (int i = 0; i < bottomNavMenu.size(); i++) {
+            bottomNavMenu.getItem(i).setChecked(false);
+        }
+
+        if ( id == R.id.bottom_nav_home) {
+            loadFragment(new HomeFragment());
+            bottomNavigationView.getMenu().findItem(R.id.bottom_nav_home).setChecked(true);
+        }
+        else if (id == R.id.bottom_nav_add) {
+            loadFragment(new AddProductFragment());
+            bottomNavigationView.getMenu().findItem(R.id.bottom_nav_add).setChecked(true);
+        }
+        else if (id == R.id.bottom_nav_stock) {
+            loadFragment(new StockFragment());
+            bottomNavigationView.getMenu().findItem(R.id.bottom_nav_stock).setChecked(true);
+        }
+        else if (id == R.id.bottom_nav_report) {
+            loadFragment(new ReportFragment());
+            bottomNavigationView.getMenu().findItem(R.id.bottom_nav_report).setChecked(true);
+        }
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        return true;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
+    }
+}
